9965d5a791594f9c2a62683853632d45ac544ac3
Fathaah <abdulfta12@gmail.com>
2025-02-03 02:24:58 +0000

    Relies on local model path and disabled auto-download

--- eva_clip/factory.py
+++ eva_clip/factory.py
@@ -274,12 +274,8 @@ def create_model(
 
         pretrained_cfg = {}
         if pretrained:
-            checkpoint_path = ''
+            checkpoint_path = os.path.join(local_dir, 'EVA02_CLIP_L_336_psz14_s6B.pt')
             pretrained_cfg = get_pretrained_cfg(model_name, pretrained)
-            if pretrained_cfg:
-                checkpoint_path = download_pretrained(pretrained_cfg, cache_dir=cache_dir, local_dir=local_dir)
-            elif os.path.exists(pretrained):
-                checkpoint_path = pretrained
 
             if checkpoint_path:
                 logging.info(f'Loading pretrained {model_name} weights ({pretrained}).')
